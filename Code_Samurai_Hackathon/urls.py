2bfac6b5ad1cf6678d1022fe9f69f02cc80e10f2
Muhammad Nasif Imtiaz <1705109@ugrad.cse.buet.ac.bd>
2022-12-18 13:26:17 +0600

    Base Project Created

--- Code_Samurai_Hackathon/urls.py
+++ Code_Samurai_Hackathon/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+import observer.views as obsever_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+    path('', obsever_views.load),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
